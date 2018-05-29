0bd25d7abff72d76eff7c6d998fec7b284694bf9
ripee <ripee.dev@gmail.com>
2018-05-29 09:21:30 +0200

    Fix vendor image mismatch
    Set matching build fingerprint for system, vendor and boot images.

--- lineage.mk
+++ lineage.mk
@@ -23,4 +23,20 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_DEVICE := tblte
 PRODUCT_NAME := lineage_tblte
 
-BUILD_FINGERPRINT=samsung/tbltebtu/tblte:6.0.1/MMB29M/N915FYXXS1DQH2:user/release-keys
+ifeq ($(filter tbltexx,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tbltebtu/tblte:6.0.1/MMB29M/N915FYXXS1DQH2:user/release-keys
+else ifeq ($(filter tbltecan,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tbltecan/tblte:6.0.1/MMB29M/N915W8VLS1DQD1:user/release-keys
+else ifeq ($(filter tbltedt,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tbltedt/tblte:6.0.1/MMB29M/N915GDTS1DQE1:user/release-keys
+else ifeq ($(filter tbltespr,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tbltespr/tblte:6.0.1/MMB29M/N915PVPS4DQC1:user/release-keys
+else ifeq ($(filter tbltetmo,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tbltetmo/tblte:6.0.1/MMB29M/N915TUVS2EQE2:user/release-keys
+else ifeq ($(filter tblteusc,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tblteusc/tblte:6.0.1/MMB29M/N915R4TYS1CQC1:user/release-keys
+else ifeq ($(filter tbltevzw,$(TARGET_DEVICE)),)
+    BUILD_FINGERPRINT=samsung/tbltevzw/tblte:6.0.1/MMB29M/N915VVRS2CQE1:user/release-keys
+else
+    BUILD_FINGERPRINT=samsung/tbltebtu/tblte:6.0.1/MMB29M/N915FYXXS1DQH2:user/release-keys
+endif
